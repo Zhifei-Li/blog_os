c72290df982041180445bf78a3935b3216466a93
Zhifei-Li <113303904+Zhifei-Li@users.noreply.github.com>
2024-12-08 22:20:43 -0500

    VGA

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,15 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
-
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+panic!("Some panic message");
     loop {}
 }
+
