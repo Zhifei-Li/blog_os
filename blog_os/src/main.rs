7a3d847d971001ad3be22a1820200bd8cae01e05
Zhifei-Li <113303904+Zhifei-Li@users.noreply.github.com>
2024-12-09 17:07:48 -0500

    创建新映射

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -10,15 +10,24 @@ use bootloader::{BootInfo, entry_point};
 
 entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello World{}", "!");
 
+    use x86_64::structures::paging::PageTable;
+    use blog_os::memory::translate_addr;
+    use blog_os::memory;
+    use x86_64::{structures::paging::Page, structures::paging::Translate, VirtAddr}; 
+
+    println!("Hello World{}", "!");
     blog_os::init();
 
     use x86_64::registers::control::Cr3;
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
-
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     // as before
     #[cfg(test)]
--- blog_os/src/memory.rs
+++ blog_os/src/memory.rs
@@ -1,16 +1,44 @@
 // in src/memory.rs
-
+use x86_64::structures::paging::OffsetPageTable;
 use x86_64::{
-    structures::paging::PageTable,
     VirtAddr,
 };
+use x86_64::{
+    PhysAddr,
+    structures::paging::{PageTable, Page, PhysFrame, Mapper, Size4KiB, FrameAllocator}
+};
 
-/// 返回一个对活动的4级表的可变引用。
-///
-/// 这个函数是不安全的，因为调用者必须保证完整的物理内存在传递的 
-/// `physical_memory_offset`处被映射到虚拟内存。另外，这个函数
-/// 必须只被调用一次，以避免别名"&mut "引用（这是未定义的行为）。
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: 这并不安全，我们这样做只是为了测试。
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     -> &'static mut PageTable
 {
     use x86_64::registers::control::Cr3;
@@ -22,4 +50,44 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // unsafe
+}
+
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr)
+    -> Option<PhysAddr>
+{
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
+    -> Option<PhysAddr>
+{
+    use x86_64::structures::paging::page_table::FrameError;
+    use x86_64::registers::control::Cr3;
+
+    // 从CR3寄存器中读取活动的4级 frame
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
+    ];
+    let mut frame = level_4_table_frame;
+
+    // 遍历多级页表
+    for &index in &table_indexes {
+        // 将该框架转换为页表参考
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe {&*table_ptr};
+
+        // 读取页表条目并更新`frame`。
+        let entry = &table[index];
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
+        };
+    }
+
+    // 通过添加页面偏移量来计算物理地址
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
