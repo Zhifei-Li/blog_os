11273cc47ac07d136ebca08b21ed306ecdfb2407
Zhifei-Li <113303904+Zhifei-Li@users.noreply.github.com>
2024-12-08 22:36:22 -0500

    test

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -1,19 +1,31 @@
 
-#![no_std] // 不链接 Rust 标准库
-#![no_main] // 禁用所有 Rust 层级的入口点
+#![no_std] 
+#![no_main] 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+#[no_mangle]
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-    loop {}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
